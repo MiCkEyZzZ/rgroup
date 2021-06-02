a0c53c69b806f44ce3d275f7f6bd973e285a45c8
Михаил Журавлев <mikhail.f.zhuravlev@gmail.com>
2021-06-03 04:09:53 +0500

    add a status color

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -21,12 +21,6 @@ interface IListProps {
 const List: React.FC<IListProps> = ({characters, error}) => {
     const addUserFavorite = (id: number) => {}
 
-    const statusColor = {
-        alive: 'alive',
-        dead: 'dead',
-        unknown: 'unknown',
-    }
-
     if (error) {
         return (
             <div className="list">
@@ -40,50 +34,61 @@ const List: React.FC<IListProps> = ({characters, error}) => {
             <div className="list">
                 <ul className="list-table">
                     {characters && characters.map((character, index) => (
-                            <li
-                                key={`{${character.id}_${index}`}
-                                className="list-table-item"
-                            >
-                                <div className="list-table-item__info">
-                                    <ul className="list-table-item__info-list">
-                                        <li className='list-table-item__info-item'>
-                                            <div className='list-table-item__info-avatar'>
-                                                <img className='list-table-item__info-avatar--image' src={character.image} alt={character.name} />
-                                            </div>
-                                        </li>
-                                        <li className='list-table-item__info-item list-table-item__user'>
-                                            <div className="list-table-item__user-title">
-                                                <h3 className='list-table-item__user-title--text'>{makeShortTitleText(character.name)}</h3>
-                                                <span className='list-table-item__user-status'>
-                                                    <span className={`list-table-item__user-status--icon list-table-item__user-status--${statusColor.dead}`}></span>
-                                                    <p className='list-table-item__user-status--text'>{character.status}  -  {character.species}</p>
+                        <li
+                            key={`{${character.id}_${index}`}
+                            className="list-table-item"
+                        >
+                            <div className="list-table-item__info">
+                                <ul className="list-table-item__info-list">
+                                    <li className='list-table-item__info-item'>
+                                        <div className='list-table-item__info-avatar'>
+                                            <img className='list-table-item__info-avatar--image' src={character.image} alt={character.name} />
+                                        </div>
+                                    </li>
+                                    <li className='list-table-item__info-item list-table-item__user'>
+                                        <div className="list-table-item__user-title">
+                                            <h3 className='list-table-item__user-title--text'>{makeShortTitleText(character.name)}</h3>
+                                            <span className='list-table-item__user-status'>
+                                                    {character.status === 'Alive'
+                                                        ? <span className='list-table-item__user-status--icon list-table-item__user-status--alive'>
+                                                            <span className='hidden'>Alive</span>
+                                                          </span>
+                                                        : character.status === 'Dead'
+                                                            ? <span className='list-table-item__user-status--icon list-table-item__user-status--dead'>
+                                                            <span className='hidden'>Dead</span>
+                                                          </span>
+                                                            : <span className='list-table-item__user-status--icon list-table-item__user-status--unknown'>
+                                                            <span className='hidden'>Unknown</span>
+                                                          </span>
+                                                    }
+                                                <p className='list-table-item__user-status--text'>{character.status}  -  {character.species}</p>
                                                 </span>
-                                            </div>
-                                            <div className="list-table-item__user-loc">
-                                                <span className="list-table-item__user-loc--title">Last known location:</span>
-                                                {character.location.name}
-                                            </div>
-                                            <div className="list-table-item__user-loc">
-                                                <span className="list-table-item__user-loc--title">Gender:</span>
-                                                {character.gender}
-                                            </div>
-                                            <div className='list-table-item__user-btn'>
-                                                <ButtonCharacters
-                                                    title='Add Favorite'
-                                                    className='btn'
-                                                    onClickButton={() => addUserFavorite(character.id)}
-                                                />
-                                                <LinkCharacters
-                                                    title='Show more'
-                                                    className='link'
-                                                    link={`/personages/personage/${character.id}`}
-                                                />
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                        ))
+                                        </div>
+                                        <div className="list-table-item__user-loc">
+                                            <span className="list-table-item__user-loc--title">Last known location:</span>
+                                            {character.location.name}
+                                        </div>
+                                        <div className="list-table-item__user-loc">
+                                            <span className="list-table-item__user-loc--title">Gender:</span>
+                                            {character.gender}
+                                        </div>
+                                        <div className='list-table-item__user-btn'>
+                                            <ButtonCharacters
+                                                title='Add Favorite'
+                                                className='btn'
+                                                onClickButton={() => addUserFavorite(character.id)}
+                                            />
+                                            <LinkCharacters
+                                                title='Show more'
+                                                className='link'
+                                                link={`/personages/personage/${character.id}`}
+                                            />
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    ))
                     }
                 </ul>
             </div>
